82c885727feb632756a6b9582b0da97fd558d8ce
tabarry <tahir@sulata.com.pk>
2020-11-08 06:49:59 +0500

    Fixed missing file and path

--- blank-project/_admin/includes/footer-js.php
+++ blank-project/_admin/includes/footer-js.php
@@ -10,7 +10,7 @@
 <!-- Framework JS -->
 <script type="text/javascript" src="<?php echo BASE_URL; ?>sulata/js/framework.js"></script>
 <!-- This Site JS -->
-<link href="<?php echo BASE_URL; ?>site-assets/js/site.js" rel="stylesheet" type="text/css"/>
+<script type="text/javascript" src="<?php echo BASE_URL; ?>site-assets/js/site.js"></script>
 <!-- JQUI -->
 <script src="<?php echo BASE_URL; ?>sulata/jquery-ui/js/jquery-ui-1.9.2.custom.min.js" type="text/javascript"></script>
 <!-- CK Editor -->
